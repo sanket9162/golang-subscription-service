2757731686983e6c88e38558cdd1a1bd9c90df52
sanket9162 <sanketgondhali55@gmail.com>
2025-04-28 16:33:24 +0530

    add database package & database models

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"subscription-service/data"
 	"sync"
 	"syscall"
 	"time"
@@ -45,6 +46,7 @@ func main() {
 		InfoLog:  infoLog,
 		ErrorLog: errorLog,
 		Wait:     &wg,
+		Models:   data.New(db),
 	}
 	// set up mail
 
